a24b70d39be4017ef7ec0b093dba1fed695568b0
Grant Handy <grantshandy@gmail.com>
2019-04-27 21:22:54 -0600

    add dock script

--- setup.sh
+++ setup.sh
@@ -14,4 +14,11 @@ sudo apt install arc-theme
 sudo cp mountains_lake_tops_129263_3840x2160.jpg /usr/share/backgrounds
 # set background "mountains_lake_tops_129263_3840x2160.jpg"
 gsettings set org.gnome.desktop.background picture-uri file:///usr/share/backgrounds/mountains_lake_tops_129263_3840x2160.jpg
-
+# set dock settings
+gsettings set org.gnome.shell.extensions.dash-to-dock show-apps-at-top true
+gsettings set org.gnome.shell.extensions.dash-to-dock running-indicator-style DEFAULT
+gsettings set org.gnome.shell.extensions.dash-to-dock custom-theme-customize-running-dots false
+gsettings set org.gnome.shell.extensions.dash-to-dock custom-theme-shrink false
+gsettings set org.gnome.shell.extensions.dash-to-dock transparency-mode DEFAULT
+gsettings set org.gnome.shell.extensions.dash-to-dock extend-height false
+gsettings set org.gnome.shell.extensions.dash-to-dock dock-position BOTTOM
