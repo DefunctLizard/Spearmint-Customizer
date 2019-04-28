4d8c4997fa7cfffb990f232faf14c16cdad9e596
Grant Handy <grantshandy@gmail.com>
2019-04-28 10:00:20 -0600

    Add files via upload

---
+++ uninstall.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+# Created By Grant Handy
+# SPEARMINT CUSTOMIZER (UNINSTALLER)
+
+# THIS SCRIPT IS INTENDED FOR GNOME VERSIONS 3.30 - 3.32
+
+# move background to /usr/share/backgrounds
+sudo rm /usr/share/backgrounds/mountains_lake_tops_129263_3840x2160.jpg
+
+# set background "mountains_lake_tops_129263_3840x2160.jpg"
+gsettings set org.gnome.desktop.background picture-uri file:///usr/share/backgrounds/warty-final-ubuntu.png
+
+# set dock settings
+dconf reset -f /org/gnome/shell/extensions/dash-to-dock/
+
+# uninstall git
+sudo apt-get purge git
+
+# uninstall xz-utils
+sudo apt-get purge xz-utils
+
+# uninstall gnome-shell-extensions
+sudo apt-get purge gnome-shell-extensions
+
+# uninstall gnome-tweaks
+sudo apt-get purge gnome-tweaks
+
+# uninstall arc-theme
+sudo apt-get purge arc-theme
