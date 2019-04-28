413e77933b6aa5bb1a9d67b55593ec4bc295b9a4
Grant Handy <grantshandy@gmail.com>
2019-04-27 22:07:58 -0600

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,16 +4,24 @@
 
 # THIS SCRIPT IS INTENDED FOR GNOME VERSIONS 3.30 - 3.32!
 
+# install git
+sudo apt-get install git
+
 # install gnome-tweaks
 sudo apt-get install gnome-tweak-tool
-# install gnome-shell-extensions
+
+#install gnome-shell-extensions
 sudo apt install gnome-shell-extensions
+
 # install arc-theme
 sudo apt install arc-theme
+
 # move background to /usr/share/backgrounds
 sudo cp mountains_lake_tops_129263_3840x2160.jpg /usr/share/backgrounds
+
 # set background "mountains_lake_tops_129263_3840x2160.jpg"
 gsettings set org.gnome.desktop.background picture-uri file:///usr/share/backgrounds/mountains_lake_tops_129263_3840x2160.jpg
+
 # set dock settings
 gsettings set org.gnome.shell.extensions.dash-to-dock show-apps-at-top true
 gsettings set org.gnome.shell.extensions.dash-to-dock running-indicator-style DEFAULT
@@ -22,4 +30,6 @@ gsettings set org.gnome.shell.extensions.dash-to-dock custom-theme-shrink false
 gsettings set org.gnome.shell.extensions.dash-to-dock transparency-mode DEFAULT
 gsettings set org.gnome.shell.extensions.dash-to-dock extend-height false
 gsettings set org.gnome.shell.extensions.dash-to-dock dock-position BOTTOM
+
 # install dash to dock
+git clone https://github.com/micheleg/dash-to-dock.git
