dbdc1016cf3bd1d52bf86e1fc01e2dafc0c3f4e1
Grant Handy <grantshandy@gmail.com>
2019-04-30 17:06:14 -0600

    Add files via upload

---
+++ package_selector.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+zenity --list --checklist --title="Choose Packages to Install" --width="1000" --height="400" \
+--column="Select" --column="Package Name" --column="Description" \
+" " GIMP "Known as GIMP, GNU Image Manipulation Program is a free and open source photo editor." \
+" " neofetch "A command-line system information tool." \
+" " caffeine "A status bar application able to temporarily prevent the activation of both the screensaver and the sleep powersaving mode." \
+" " bleachbit "BleachBit is a free and open-source disk space cleaner, privacy manager, and computer system optimizer." \
+" " vlc-media-player "VLC media player is a free and open-source, portable, cross-platform media player and streaming media server." \
+" " vim-editor "VIM is a free and open-source command-line text editor" \
+" " inkscape "Inkscape is a free and open-source vector graphics editor." \
+
