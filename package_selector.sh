292f07eeec1c5ae3474fd4eaf8c1f6d266f572ba
Grant Handy <grantshandy@gmail.com>
2019-05-05 20:28:52 -0600

    Update package_selector.sh

--- package_selector.sh
+++ package_selector.sh
@@ -1,3 +1,18 @@
+#!/bin/bash
+   #################################################################
+   #                                                               #
+   #                 Spearmint Customizer                          #
+   #                Created By Grant Handy                         #
+   #       Licensed under the GNU General Public License 3.0       #
+   #                grantshandy@gmail.com                          #
+   #     https://github.com/DefunctLizard/spearmint-customizer     #
+   #                                                               #
+   #################################################################
+
+# update packages
+sudo apt-get update
+
+# open dialog boxes
 zenityResult=$(
   zenity --list --checklist --title="Choose Packages to Install" --width="1000" --height="400" \
          --column="Select" --column="Package Name" --column="Description" \
@@ -20,29 +35,59 @@ fi
 
 # Wildcards (*) around the result will find matches.
 if [[ $zenityResult == *"gimp"* ]]; then
-  sudo apt-get install gimp
+  sudo apt-get install gimp -yy
 fi
 
 if [[ $zenityResult == *"neofetch"* ]]; then
-  sudo apt-get install neofetch
+  sudo apt install neofetch -yy
 fi
 
 if [[ $zenityResult == *"caffeine"* ]]; then
-  sudo apt-get install caffeine
+  sudo apt-get install caffeine -yy
 fi
 
 if [[ $zenityResult == *"bleachbit"* ]]; then
-  sudo apt-get install bleachbit
+  sudo apt-get install bleachbit -yy
 fi
 
 if [[ $zenityResult == *"vlc-media-player"* ]]; then
-  sudo apt-get install vlc-media-player
+  sudo apt-get install vlc -yy
 fi
 
 if [[ $zenityResult == *"vim-editor"* ]]; then
-  sudo apt-get install vim-editor
+  sudo apt-get install vim -yy
 fi
 
 if [[ $zenityResult == *"inkscape"* ]]; then
-  sudo apt-get install inkscape
+  sudo apt-get install inkscape -yy
 fi
+
+# Edit .bashrc for neofetch
+echo "neofetch" >> ~/.bashrc
+
+# switch to arc-darker
+gsettings set org.gnome.desktop.interface gtk-theme "Arc-Darker"
+
+# move background to /usr/share/backgrounds
+sudo cp mountains_lake_tops_129263_3840x2160.jpg /usr/share/backgrounds
+
+# set background "mountains_lake_tops_129263_3840x2160.jpg"
+gsettings set org.gnome.desktop.background picture-uri file:///usr/share/backgrounds/mountains_lake_tops_129263_3840x2160.jpg
+
+# set dock settings
+gsettings set org.gnome.shell.extensions.dash-to-dock show-apps-at-top true
+gsettings set org.gnome.shell.extensions.dash-to-dock running-indicator-style DEFAULT
+gsettings set org.gnome.shell.extensions.dash-to-dock custom-theme-customize-running-dots false
+gsettings set org.gnome.shell.extensions.dash-to-dock custom-theme-shrink false
+gsettings set org.gnome.shell.extensions.dash-to-dock transparency-mode DEFAULT
+gsettings set org.gnome.shell.extensions.dash-to-dock extend-height false
+gsettings set org.gnome.shell.extensions.dash-to-dock dock-position BOTTOM
+
+# open the flat-remix-blue icons
+sudo tar -xf Flat-Remix-Blue_1.90.tar.xz
+
+# copy flat-remix-blue icons
+sudo cp -r Flat-Remix-Blue /usr/share/icons
+
+# set icon theme
+gsettings set org.gnome.desktop.interface icon-theme "Flat-Remix-Blue"
