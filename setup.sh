dfe9357e607054f421918afc3b196504293f3997
Grant Handy <grantshandy@gmail.com>
2019-05-06 16:40:18 -0600

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -12,7 +12,7 @@
 # update packages
 sudo apt-get update
 
-# open dialog box for package manager
+# open dialog boxes
 zenityResult=$(
   zenity --list --checklist --title="Choose Packages to Install" --width="1000" --height="400" \
          --column="Select" --column="Package Name" --column="Description" \
@@ -20,20 +20,18 @@ zenityResult=$(
          " " neofetch "A command-line system information tool." \
          " " caffeine "A status bar application able to temporarily prevent the activation of both the screensaver and the sleep powersaving mode." \
          " " bleachbit "BleachBit is a free and open-source disk space cleaner, privacy manager, and computer system optimizer." \
-         " " vlc-media-player "VLC media player is a free and open-source, portable, cross-platformsudo apt-get install atom media player and streaming media server." \
+         " " vlc-media-player "VLC media player is a free and open-source, portable, cross-platform media player and streaming media server." \
          " " vim-editor "VIM is a free and open-source command-line text editor" \
          " " inkscape "Inkscape is a free and open-source vector graphics editor." \
 )
 
-if [[ $zenityResult == " " ]]; then
-" "
-else
-  zenity --question --width="500" --text="Are you sure you wish to install these packages: $zenityResult"
-  confirmCode=$?
+zenity --question --text="Are you sure you wish to install these packages: $zenityResult"
+confirmCode=$?
 
-  if [ "$confirmCode" -ne 0 ]; then
-    echo "Cancelled. Thank you."
-  fi
+if [ "$confirmCode" -ne 0 ]; then
+  echo "Cancelled. Thank you."
+  exit 1
+fi
 
 # Wildcards (*) around the result will find matches.
 if [[ $zenityResult == *"gimp"* ]]; then
@@ -42,7 +40,6 @@ fi
 
 if [[ $zenityResult == *"neofetch"* ]]; then
   sudo apt install neofetch -yy
-  echo "neofetch" >> ~/.bashrc
 fi
 
 if [[ $zenityResult == *"caffeine"* ]]; then
@@ -64,3 +61,6 @@ fi
 if [[ $zenityResult == *"inkscape"* ]]; then
   sudo apt-get install inkscape -yy
 fi
+
+# Edit .bashrc for neofetch
+echo "neofetch" >> ~/.bashrc
