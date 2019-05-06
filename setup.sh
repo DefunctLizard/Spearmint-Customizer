653f5c930614d265c3a27dbcee74465f750e44e4
Grant Handy <grantshandy@gmail.com>
2019-05-05 20:29:17 -0600

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -12,19 +12,55 @@
 # update packages
 sudo apt-get update
 
-# install various programs
-sudo apt install neofetch -yy
-sudo apt-get install xz-utils -yy
-sudo apt install gnome-tweak-tool -yy
-sudo apt-get install gnome-shell-extensions -yy 
-sudo apt-get install bleachbit -yy
-sudo apt-get install vlc -yy
-sudo apt install arc-theme -yy
-sudo apt-get install git -yy
-sudo apt install vim -yy
-sudo apt-get install gimp -yy
-sudo apt-get install inkscape -yy
-sudo apt-get install caffeine -yy
+# open dialog boxes
+zenityResult=$(
+  zenity --list --checklist --title="Choose Packages to Install" --width="1000" --height="400" \
+         --column="Select" --column="Package Name" --column="Description" \
+         " " gimp "Known as GIMP, GNU Image Manipulation Program is a free and open source photo editor." \
+         " " neofetch "A command-line system information tool." \
+         " " caffeine "A status bar application able to temporarily prevent the activation of both the screensaver and the sleep powersaving mode." \
+         " " bleachbit "BleachBit is a free and open-source disk space cleaner, privacy manager, and computer system optimizer." \
+         " " vlc-media-player "VLC media player is a free and open-source, portable, cross-platform media player and streaming media server." \
+         " " vim-editor "VIM is a free and open-source command-line text editor" \
+         " " inkscape "Inkscape is a free and open-source vector graphics editor." \
+)
+
+zenity --question --text="Are you sure you wish to install these packages: $zenityResult"
+confirmCode=$?
+
+if [ "$confirmCode" -ne 0 ]; then
+  echo "Cancelled. Thank you."
+  exit 1
+fi
+
+# Wildcards (*) around the result will find matches.
+if [[ $zenityResult == *"gimp"* ]]; then
+  sudo apt-get install gimp -yy
+fi
+
+if [[ $zenityResult == *"neofetch"* ]]; then
+  sudo apt install neofetch -yy
+fi
+
+if [[ $zenityResult == *"caffeine"* ]]; then
+  sudo apt-get install caffeine -yy
+fi
+
+if [[ $zenityResult == *"bleachbit"* ]]; then
+  sudo apt-get install bleachbit -yy
+fi
+
+if [[ $zenityResult == *"vlc-media-player"* ]]; then
+  sudo apt-get install vlc -yy
+fi
+
+if [[ $zenityResult == *"vim-editor"* ]]; then
+  sudo apt-get install vim -yy
+fi
+
+if [[ $zenityResult == *"inkscape"* ]]; then
+  sudo apt-get install inkscape -yy
+fi
 
 # Edit .bashrc for neofetch
 echo "neofetch" >> ~/.bashrc
