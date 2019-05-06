4ef9970d9c33a837d5d9c9c62bf0d9a759762166
DefunctLizard <45475651+DefunctLizard@users.noreply.github.com>
2019-05-06 17:05:04 -0600

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -9,20 +9,22 @@
    #                                                               #
    #################################################################
 
-# update packages
+# update packages and install essentials
 sudo apt-get update
+sudo apt-get install xz-utils
+sudo apt-get install zenity
 
 # open dialog boxes
 zenityPackageResult=$(
   zenity --list --checklist --title="Choose Packages to Install" --width="1000" --height="400" \
          --column="Select" --column="Package Name" --column="Description" \
-         " " gimp "Known as GIMP, GNU Image Manipulation Program is a free and open source photo editor." \
-         " " neofetch "A command-line system information tool." \
-         " " caffeine "A status bar application able to temporarily prevent the activation of both the screensaver and the sleep powersaving mode." \
-         " " bleachbit "BleachBit is a free and open-source disk space cleaner, privacy manager, and computer system optimizer." \
-         " " vlc-media-player "VLC media player is a free and open-source, portable, cross-platform media player and streaming media server." \
-         " " vim-editor "VIM is a free and open-source command-line text editor" \
-         " " inkscape "Inkscape is a free and open-source vector graphics editor." \
+         " " "gimp" "Known as GIMP, GNU Image Manipulation Program is a free and open source photo editor." \
+         " " "neofetch" "A command-line system information tool." \
+         " " "caffeine" "A status bar application able to temporarily prevent the activation of both the screensaver and the sleep powersaving mode." \
+         " " "bleachbit" "BleachBit is a free and open-source disk space cleaner, privacy manager, and computer system optimizer." \
+         " " "vlc-media-player" "VLC media player is a free and open-source, portable, cross-platform media player and streaming media server." \
+         " " "vim-editor" "VIM is a free and open-source command-line text editor" \
+         " " "inkscape" "Inkscape is a free and open-source vector graphics editor." \
 )
 
 # Wildcards (*) around the result will find matches.
@@ -58,7 +60,7 @@ fi
 zenityThemeResult=$(
   zenity --list --checklist --title="Choose Packages to Install" --width="1000" --height="400" \
          --column="Select" --column="Package Name" --column="Description" \
-         " " "default-theme" "Known as GIMP, GNU Image Manipulation Program is a free and open source photo editor." \
+         " " "default-theme" "The default theme looks great (improve this)." \
 )
 
 if [[ $zenityThemeResult == *"default-theme"* ]]; then
