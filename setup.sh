9d5c13f25371144de05f7e6b41f92d9386c19a1e
DefunctLizard <45475651+DefunctLizard@users.noreply.github.com>
2019-05-08 20:49:41 -0600

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -71,6 +71,7 @@ zenityThemeResult=$(
          "macOS-dark" "This attempts to emulate the look and feel of macOSX mojave dark." \
          "macOS-light" "This attempts to emulate the look and feel of macOSX mojave light." \
          "qogir-win" "qogir is a blue theme with windows 10 like borders." \
+         "materia" "The materia theme is a great material theme that incorporates the tela icon theme. "
 )
 
 if [[ $zenityThemeResult == "spearmint-default" ]]; then
@@ -161,3 +162,16 @@ if [[ $zenityThemeResult == "qogir-win" ]]; then
   sudo cp -r Qogir /usr/share/icons
   gsettings set org.gnome.desktop.interface icon-theme "Qogir"
 fi
+
+if [[ $zenityThemeResult == "materia" ]]; then
+  sudo apt-get update
+  sudo apt-get install materia
+  gsettings set org.gnome.desktop.interface gtk-theme "Materia"
+  git clone https://github.com/DefunctLizard/materia-GNOME.git
+  cd /$HOME/spearmint-customizer/materia-GNOME/
+  sudo tar -xf Tela-blue.tar.xz
+  sudo cp -r Tela-blue /usr/share/themes
+  dconf reset -f /org/gnome/shell/extensions/dash-to-dock/
+  gsettings set org.gnome.desktop.wm.preferences button-layout ':close,maximize,minimize'
+  gsettings set org.gnome.shell.extensions.dash-to-dock dock-position BOTTOM
+  gsettings set org.gnome.shell.extensions.dash-to-dock show-apps-at-top true
