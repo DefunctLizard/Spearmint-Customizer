90b6b6e89ea7ba8a4135f3232f54e2a7a7ce547b
DefunctLizard <45475651+DefunctLizard@users.noreply.github.com>
2019-05-08 21:00:39 -0600

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -165,12 +165,12 @@ fi
 
 if [[ $zenityThemeResult == "materia" ]]; then
   sudo apt-get update
-  sudo apt-get install materia
+  sudo apt-get install materia-gtk-theme
   gsettings set org.gnome.desktop.interface gtk-theme "Materia"
   git clone https://github.com/DefunctLizard/materia-GNOME.git
   cd /$HOME/spearmint-customizer/materia-GNOME/
   sudo tar -xf Tela-blue.tar.xz
-  sudo cp -r Tela-blue /usr/share/themes
+  sudo cp -r Tela-blue /usr/share/icons
   gsettings set org.gnome.desktop.interface icon-theme "Tela-blue"
   dconf reset -f /org/gnome/shell/extensions/dash-to-dock/
   gsettings set org.gnome.desktop.wm.preferences button-layout ':close,maximize,minimize'
