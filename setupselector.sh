77e6dcf70fba5296c379e234fbb2fa8a1fec1a7f
DefunctLizard <45475651+DefunctLizard@users.noreply.github.com>
2019-05-12 12:14:41 -0600

    Update setupselector.sh

--- setupselector.sh
+++ setupselector.sh
@@ -4,7 +4,7 @@ DISTRIBUTION=unknown
 
 DISTRIBUTION=$( cat /etc/os-release )
 
-if [[ $DISTRIBUTION == *"PRETTY_NAME="Ubuntu 19.04"* ]]; then
+if [[ $DISTRIBUTION == *"PRETTY_NAME="Ubuntu 19.04""* ]]; then
   cd /$HOME/spearmint-customizer/Ubuntu/19.04/
   chmod +x setup.sh
   ./setup.sh
