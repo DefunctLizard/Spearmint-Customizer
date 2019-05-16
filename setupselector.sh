012d861ba41f0f4170f185d0c9fc7888559179b0
Grant Handy <grantshandy@gmail.com>
2019-05-16 09:39:28 -0600

    Update setupselector.sh

--- setupselector.sh
+++ setupselector.sh
@@ -10,7 +10,7 @@ if [[ $DISTRIBUTION == *"Ubuntu 19.04"* ]]; then
   ./setup.sh
 fi
 
-if [[ $DISTRIBUTION == *"Debian GNU/Linux 9 (stretch)"* ]]; then
+if [[ $DISTRIBUTION == *"Debian"* ]]; then
   cd /$HOME/spearmint-customizer/Debian/9/
   chmod +x setup.sh
   ./setup.sh
