7905b606e875af74f34f43cb0c481cc659a4f73d
DefunctLizard <45475651+DefunctLizard@users.noreply.github.com>
2019-05-15 20:33:05 -0600

    Update setupselector.sh

--- setupselector.sh
+++ setupselector.sh
@@ -5,13 +5,13 @@ DISTRIBUTION=unknown
 DISTRIBUTION=$( cat /etc/os-release )
 
 if [[ $DISTRIBUTION == *"Ubuntu 19.04"* ]]; then
-  cd /$HOME/spearmint-customizer/Ubuntu/9/
+  cd /$HOME/spearmint-customizer/Ubuntu/19.04/
   chmod +x setup.sh
   ./setup.sh
 fi
 
 if [[ $DISTRIBUTION == *"Debian GNU/Linux 9 (stretch)"* ]]; then
-  cd /$HOME/spearmint-customizer/Debian/19.04/
+  cd /$HOME/spearmint-customizer/Debian/9/
   chmod +x setup.sh
   ./setup.sh
 fi
