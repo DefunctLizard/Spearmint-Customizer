e4e89c53f92a8d6713d5e9f66ef11e35e7a73e8f
DefunctLizard <45475651+DefunctLizard@users.noreply.github.com>
2019-05-21 20:56:45 -0600

    Update setupselector.sh

--- setupselector.sh
+++ setupselector.sh
@@ -8,26 +8,25 @@ GNOME_VER=$(gnome-shell --version)
 
 DISTRIBUTION=$( cat /etc/os-release )
 
-if [[ $DISTRIBUTION == *"Ubuntu 19.04"* ]]; then
-  cd /$HOME/spearmint-customizer/Ubuntu/19.04/
-  chmod +x setup.sh
-  ./setup.sh
-fi
-
-if [[ $DISTRIBUTION == *"Debian"* ]]; then
-  cd /$HOME/spearmint-customizer/Debian/9/
-  chmod +x setup.sh
-  ./setup.sh
-fi
-
-if [[ $DISTRIBUTION == *"Debian"* ]]; then
-  cd /$HOME/spearmint-customizer/Debian/9/
-  chmod +x setup.sh
-  ./setup.sh
-fi
-
-if [[ $GNOME_VER == *"GNOME"* ]]; then
+if [[ $GNOME_VER == *"gnome"* ]]; then
   echo "GNOME is detected"
+  if [[ $DISTRIBUTION == *"Ubuntu 19.04"* ]]; then
+    cd /$HOME/spearmint-customizer/Ubuntu/19.04/
+    chmod +x setup.sh
+    ./setup.sh
+  fi
+
+  if [[ $DISTRIBUTION == *"Debian"* ]]; then
+    cd /$HOME/spearmint-customizer/Debian/9/
+    chmod +x setup.sh
+    ./setup.sh
+  fi
+
+  if [[ $DISTRIBUTION == *"Debian"* ]]; then
+    cd /$HOME/spearmint-customizer/Debian/9/
+    chmod +x setup.sh
+    ./setup.sh
+  fi
 else
   cd /$HOME/spearmint-customizer/Debian/Raspberry_Pi/
   chmod +x setup.sh
