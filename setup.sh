4ac731c7e9db7c0ea4e13a6b67668ac620f6a51f
DefunctLizard <45475651+DefunctLizard@users.noreply.github.com>
2019-05-26 16:51:35 -0600

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -6,43 +6,50 @@ GNOME_VER=$( gnome-shell --version )
 
 DISTRIBUTION=$( cat /etc/os-release )
 
-if [[ $GNOME_VER == *"GNOME"* ]]; then
-  echo "GNOME is detected"
-  if [[ $DISTRIBUTION == *"Ubuntu 19.04"* ]]; then
-    cd /$HOME/spearmint-customizer/Ubuntu/19.04/
-    chmod +x setup.sh
-    ./setup.sh
-  fi
-
-  if [[ $DISTRIBUTION == *"Debian"* ]]; then
-    cd /$HOME/spearmint-customizer/Debian/9/
-    chmod +x setup.sh
-    ./setup.sh
-  fi
-
-  if [[ $DISTRIBUTION == *"Ubuntu 18.04.2"* ]]; then
-    cd /$HOME/spearmint-customizer/Ubuntu/18.04.2/
-    chmod +x setup.sh
-    ./setup.sh
-  fi
-
+if [[ $DISTRIBUTION == *"cat: /etc/os-release: No such file or directory"* ]]; then
+  echo "starting macOS setup"
+  cd /$HOME/spearmint-customizer/macOS
+  chmod +x setup.sh
+  ./setup.sh
 else
-  echo "GNOME was not detected"
-  if [[ $DISTRIBUTION == *"Debian"* ]]; then
-    cd /$HOME/spearmint-customizer/Debian/gnomeless/
-    chmod +x setup.sh
-    ./setup.sh
-  fi
+  if [[ $GNOME_VER == *"GNOME"* ]]; then
+    echo "GNOME is detected"
+    if [[ $DISTRIBUTION == *"Ubuntu 19.04"* ]]; then
+      cd /$HOME/spearmint-customizer/Ubuntu/19.04/
+      chmod +x setup.sh
+      ./setup.sh
+    fi
 
-  if [[ $DISTRIBUTION == *"Ubuntu"* ]]; then
-    cd /$HOME/spearmint-customizer/Ubuntu/gnomeless/
-    chmod +x setup.sh
-    ./setup.sh
-  fi
+    if [[ $DISTRIBUTION == *"Debian"* ]]; then
+      cd /$HOME/spearmint-customizer/Debian/9/
+      chmod +x setup.sh
+      ./setup.sh
+    fi
 
-  if [[ $DISTRIBUTION == *"cat: /etc/os-release: No such file or directory"* ]]; then
-    cd /$HOME/spearmint-customizer/macOS
-    chmod +x setup.sh
-    ./setup.sh
+    if [[ $DISTRIBUTION == *"Ubuntu 18.04.2"* ]]; then
+      cd /$HOME/spearmint-customizer/Ubuntu/18.04.2/
+      chmod +x setup.sh
+      ./setup.sh
+    fi
+
+  else
+    echo "GNOME was not detected"
+    if [[ $DISTRIBUTION == *"Debian"* ]]; then
+      cd /$HOME/spearmint-customizer/Debian/gnomeless/
+      chmod +x setup.sh
+      ./setup.sh
+    fi
+
+    if [[ $DISTRIBUTION == *"Ubuntu"* ]]; then
+      cd /$HOME/spearmint-customizer/Ubuntu/gnomeless/
+      chmod +x setup.sh
+      ./setup.sh
+    fi
+
+    if [[ $DISTRIBUTION == *"cat: /etc/os-release: No such file or directory"* ]]; then
+      cd /$HOME/spearmint-customizer/macOS
+      chmod +x setup.sh
+      ./setup.sh
+    fi
   fi
 fi
