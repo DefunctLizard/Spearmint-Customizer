0ecda54dd9a89d8c7b7e3a1ac4f3fdfb68d96b77
Grant Handy <grantshandy@gmail.com>
2019-05-27 12:44:24 -0600

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -39,13 +39,45 @@ else
     chmod +x setup.sh
     ./setup.sh
   fi
+fi || cd /$HOME/spearmint-customizer/macOS/; chmod +x setup.sh; ./setup.sh#!/bin/bash
 
-  if [[ $DISTRIBUTION == *"cat: /etc/os-release: No such file or directory"* ]]; then
-    echo "starting macOS setup"
-    cd /$HOME/spearmint-customizer/macOS
+DISTRIBUTION=unknown
+
+GNOME_VER=$( gnome-shell --version )
+
+DISTRIBUTION=$( cat /etc/os-release )
+
+if [[ $GNOME_VER == *"GNOME"* ]]; then
+  echo "GNOME is detected"
+  if [[ $DISTRIBUTION == *"Ubuntu 19.04"* ]]; then
+    cd /$HOME/spearmint-customizer/Ubuntu/19.04/
     chmod +x setup.sh
     ./setup.sh
-  else
-    echo "the suspected error is correct"
   fi
-fi
+
+  if [[ $DISTRIBUTION == *"Debian"* ]]; then
+    cd /$HOME/spearmint-customizer/Debian/9/
+    chmod +x setup.sh
+    ./setup.sh
+  fi
+
+  if [[ $DISTRIBUTION == *"Ubuntu 18.04.2"* ]]; then
+    cd /$HOME/spearmint-customizer/Ubuntu/18.04.2/
+    chmod +x setup.sh
+    ./setup.sh
+  fi
+
+else
+  echo "GNOME was not detected"
+  if [[ $DISTRIBUTION == *"Debian"* ]]; then
+    cd /$HOME/spearmint-customizer/Debian/gnomeless/
+    chmod +x setup.sh
+    ./setup.sh
+  fi
+
+  if [[ $DISTRIBUTION == *"Ubuntu"* ]]; then
+    cd /$HOME/spearmint-customizer/Ubuntu/gnomeless/
+    chmod +x setup.sh
+    ./setup.sh
+  fi
+fi || cd /$HOME/spearmint-customizer/macOS/; chmod +x setup.sh; ./setup.sh
