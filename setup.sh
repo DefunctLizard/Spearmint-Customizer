ef47fe08c8e4e4fe60549a2ecff78fab57981726
DefunctLizard <45475651+DefunctLizard@users.noreply.github.com>
2019-05-28 14:01:19 -0600

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -28,18 +28,6 @@ if [[ $GNOME_VER == *"GNOME"* ]]; then
 
 else
   echo "GNOME was not detected"
-  if [[ $DISTRIBUTION == *"Debian"* ]]; then
-    cd /$HOME/spearmint-customizer/Debian/gnomeless/
-    chmod +x setup.sh
-    ./setup.sh
-  fi
-
-  if [[ $DISTRIBUTION == *"Ubuntu"* ]]; then
-    cd /$HOME/spearmint-customizer/Ubuntu/gnomeless/
-    chmod +x setup.sh
-    ./setup.sh
-  fi
-
   while true; do
       read -p "Are you running Linux? (y or n)" yn
       case $yn in
@@ -49,17 +37,6 @@ else
       esac
   done
 
-  if [[ $LINUX == "y" ]]; then
-    while true; do
-        read -p "Are you running Debian based or Ubuntu based? (d or u)" yn
-        case $yn in
-            [Dd]* ) DEBIAN=y; break;;
-            [Uu]* ) UBUNTU=y; break;;
-            * ) echo "Please answer d or u.";;
-        esac
-    done
-  fi
-
   if [[ $LINUX == "n" ]]; then
     while true; do
         read -p "Are you running macOS? (y or n)" yn
@@ -67,6 +44,17 @@ else
             [Yy]* ) cd /$HOME/spearmint-customizer/macOS; chmod +x setup.sh; ./setup.sh; break;;
             [Nn]* ) echo "could not detect OS"; exit;;
             * ) echo "Please answer y or n.";;
+        esac
+    done
+  fi
+
+  if [[ $LINUX == "y" ]]; then
+    while true; do
+        read -p "Are you running Debian based or Ubuntu based? (d or u)" yn
+        case $yn in
+            [Dd]* ) DEBIAN=y; break;;
+            [Uu]* ) UBUNTU=y; break;;
+            * ) echo "Please answer d or u.";;
         esac
     done
   fi
